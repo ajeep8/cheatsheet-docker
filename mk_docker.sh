833ea1a62c2d3ca318d0dba918fea0ea399e9c07
ajeep <ajeep8@gmail.com>
2022-08-30 12:41:16 +0800

    清理后的产品版

--- mk_docker.sh
+++ mk_docker.sh
@@ -5,3 +5,4 @@
 docker build --pull --rm --build-arg https_proxy=http://10.222.1.1:7890 -f "Dockerfile" --target cheatsheet -t cheatsheet:v0.1 .
 #docker build --pull --rm -f "Dockerfile" --target arya -t arya:v0.1 .
 
+docker build --rm -f "Dockerfile.product" --target product -t cheatsheet:v0.2 .
